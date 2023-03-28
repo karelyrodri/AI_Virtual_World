35753a20e7dbaa7f39fcc2aefee7f6f1e367e0c2
karelyrodri <106625643+karelyrodri@users.noreply.github.com>
2023-03-28 15:49:34 -0700

    Add files via upload

--- Search_Strategies/GreedyBestFirstSearch.py
+++ Search_Strategies/GreedyBestFirstSearch.py
@@ -30,26 +30,29 @@ class GreedyBestFirstSearch():
         else:
             frontier_partial_schedules = pq.PriorityQueue() 
             self.actions.shuffle_actions_list() # shuffle to give different transfers a chance to be explored
-            while (frontier_partial_schedules.size() < self.frontier_max_size):  # only considering max frontier size amount of partial schedules
-                for action in self.actions.actions_list: # size 43
-                    action_node = None
-                    # we start with a fresh copy of the last world state in the schedule
-                    # because we are gonna impose actions on it and add it to the schedule
-                    countries = copy.deepcopy(current_schedule.decisions[-1]["world_state"]) # avoiding pass by reference
-                    country = countries[self.country.name]
-                    if (action["type"] == Actions.Action_Type.TRANSORMATION):
-                        transform = action["operator"]
-                        transform_num = self.operator.random_num_of_tranforms(country, transform)
-                    if (transform_num > 0): action_node = self.operator.transform(country, transform, transform_num)
-                    elif (action["type"] == Actions.Action_Type.TRANSFER): 
-                        transfer = action["operator"]
-                        quantity = self.operator.random_num_of_resource_quantity(transfer, countries) # random number of resources to transfer
-                        if (quantity > 0): action_node = self.operator.transfer(transfer, quantity, countries) 
+        
+            idx = 0
+            while (idx < len(self.actions.actions_list) and frontier_partial_schedules.size() < self.frontier_max_size):  # only considering max frontier size amount of partial schedules
+                action = self.actions.actions_list[idx]
+                action_node = None
+                # we start with a fresh copy of the last world state in the schedule
+                # because we are gonna impose actions on it and add it to the schedule
+                countries = copy.deepcopy(current_schedule.decisions[-1]["world_state"]) # avoiding pass by reference
+                country = countries[self.country.name]
+                if (action["type"] == Actions.Action_Type.TRANSORMATION):
+                    transform = action["operator"]
+                    transform_num = self.operator.random_num_of_tranforms(country, transform)
+                if (transform_num > 0): action_node = self.operator.transform(country, transform, transform_num)
+                elif (action["type"] == Actions.Action_Type.TRANSFER): 
+                    transfer = action["operator"]
+                    quantity = self.operator.random_num_of_resource_quantity(transfer, countries) # random number of resources to transfer
+                    if (quantity > 0): action_node = self.operator.transfer(transfer, quantity, countries) 
 
-                    if (action_node != None): # we wont count actions that did not occur
-                        next_schedule = copy.deepcopy(current_schedule)
-                        next_schedule.add_next_move(country, countries, action_node)
-                        frontier_partial_schedules.push(next_schedule) # all partial schedules are of size depth bound
+                if (action_node != None): # we wont count actions that did not occur
+                    next_schedule = copy.deepcopy(current_schedule)
+                    next_schedule.add_next_move(country, countries, action_node)
+                    frontier_partial_schedules.push(next_schedule) # all partial schedules are of size depth bound
+                idx += 1
             # here is the greedy part. we only pursue the best partial scheudle to keep searcing
             best_partial_schedule = frontier_partial_schedules.peek()
             self.search(current_depth + 1, best_partial_schedule)
